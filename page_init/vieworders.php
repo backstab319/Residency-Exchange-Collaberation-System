b5ccfc29d0046ff767ed93caa776d4741f5982c7
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-03-27 06:39:59 +0530

    Update vieworders.php

--- page_init/vieworders.php
+++ page_init/vieworders.php
@@ -40,29 +40,42 @@
     <h1 class='display-4'>Orders</h1>
     </div>
 
-    <div class="container d-flex justify-content-center text-center">
+    <div class="container d-flex justify-content-center col-md-4 col-lg-4 text-center">
     
     <?php
-        showusers();
+        showusers(1);
         if($_POST["view"]){
             viewsel();
         }
-        function showusers(){
+        function showusers($val){
             global $conn;
             $sql = "SELECT DISTINCT user_id FROM business_orders";
             $result = $conn->query($sql);
+            if($result->num_rows == 0){
+                echo "<p class='lead'>There are no orders placed yet</p>";
+                exit();
+            }
             echo "
-                <div class='form-group text-center col-md-4 col-lg-4 mb-2'>
+                <div class='form-group text-center col-md-6 col-lg-6 mb-2'>
                 <form method='POST' action='vieworders.php'>
-                <label for='sel'>Select user to view order</label>
-                <select id='sel' class='form-control mb-2' name='sel'>
             ";
+            if($val == 1){
+                echo "<label for='sel'>Select user to view order</label>";
+                echo "<select id='sel' class='form-control mb-2' name='sel'>";
+            }else{
+                echo "<label for='sel'>Delete orders</label>";
+                echo "<select id='sel' class='form-control mb-2' name='del'>";
+            }
             while($row = $result->fetch_assoc()){
                 echo "<option value='".$row['user_id']."'>".$row['user_id']."</option>";
             }
-            echo "</select>
-            <input type='submit' value='View order' name='view' class='form-control mb-2'>
-            </form>
+            echo "</select>";
+            if($val == 1){
+                echo "<input type='submit' value='View order' name='view' class='form-control mb-2'>";
+            }else{
+                echo "<input type='submit' value='Delete order' name='del-sel' class='form-control mb-2'>";
+            }
+            echo "</form>
             </div>";
         }
         function viewsel(){
@@ -93,6 +106,26 @@
         }
     ?>
     </div>
+
+    <div class="container text-center col-md-6 col-lg-6">
+        <h1 class="display-4">Remove completed orders</h1>
+        <div class="container text-center d-flex justify-content-center">
+            <?php
+                showusers(0);
+                if($_POST["del-sel"]){
+                    delorder();
+                }
+                function delorder(){
+                    global $conn;
+                    $del = $_POST["del"];
+                    $sql = "DELETE FROM business_orders WHERE user_id='$del'";
+                    $conn->query($sql);
+                }
+            ?>
+        </div>
+    </div>
+
+
     
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
